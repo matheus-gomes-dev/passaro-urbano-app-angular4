0e574b9d9e3a34f82d562da4646545288f04426b
matheus-lima92 <lima.eca@gmail.com>
2018-02-16 01:12:12 -0200

    page 'restaurantes' rendered dynamically

--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -11,12 +11,15 @@ import { OfertasService } from '../ofertas.service'
 })
 export class RestaurantesComponent implements OnInit {
 
+  public ofertas: Oferta[]
+
   constructor(private ofertasService: OfertasService) {}
 
   ngOnInit() {
   	this.ofertasService.getOfertasPorCategoria('restaurante')
   		.then( (ofertas: Oferta[] ) => {
   			console.log(ofertas)
+  			this.ofertas = ofertas
   		})
   }
 
