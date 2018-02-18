fe922af297b60be741ec879389d26dc5f9740b0f
matheus-lima92 <lima.eca@gmail.com>
2018-02-18 00:32:28 -0300

    created file por api url

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,7 +1,7 @@
 import { Oferta } from './shared/oferta.model'
 import { Http } from '@angular/http'
 import { Injectable } from '@angular/core'
-
+import { URL_API } from './app.api'
 import  'rxjs/add/operator/toPromise' //para converter observable para promise
 
 
@@ -11,19 +11,19 @@ import  'rxjs/add/operator/toPromise' //para converter observable para promise
 	constructor(private http: Http){} //serviço utilizado precisa constar no construtor
 
 	public getOfertas(): Promise<Oferta[]> {
-		return this.http.get('http://localhost:3000/ofertas?destaque=true')
+		return this.http.get(`${URL_API}?destaque=true`)
 			.toPromise() //método http retorna um observable. Por hora passaremos para promise
 			.then((resposta: any) => resposta.json())
 	}
 
 	public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
-		return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+		return this.http.get(`${URL_API}?categoria=${categoria}`)
 			.toPromise()
 			.then((resposta: any) => resposta.json())
 	}
 
 	public getOfertaPorId(id: number): Promise<Oferta> {
-		return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+		return this.http.get(`${URL_API}?id=${id}`)
 			.toPromise()
 			.then((resposta: any) => resposta.json()[0])
 	}
