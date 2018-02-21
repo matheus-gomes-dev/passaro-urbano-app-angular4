55b21289dd4ef62a7a7ef698004429dafae26f59
matheus-lima92 <lima.eca@gmail.com>
2018-02-21 13:29:11 -0300

    search bar included in 'Topo' component

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { Oferta } from '../shared/oferta.model'
 import { OfertasService } from '../ofertas.service'
-import { Observable } from 'rxjs/Observable'
-import { Observer } from 'rxjs/Observer'
-import { Subscription } from 'rxjs/Subscription'
-import 'rxjs/Rx'
+// import { Observable } from 'rxjs/Observable'
+// import { Observer } from 'rxjs/Observer'
+// import { Subscription } from 'rxjs/Subscription'
+// import 'rxjs/Rx'
 
 @Component({
   selector: 'app-oferta',
@@ -15,8 +15,8 @@ import 'rxjs/Rx'
 })
 export class OfertaComponent implements OnInit, OnDestroy {
 
-  private tempoObservableSubscription: Subscription
-  private meuObservableTesteSubscription: Subscription
+  // private tempoObservableSubscription: Subscription
+  // private meuObservableTesteSubscription: Subscription
 
   public oferta: Oferta
 
@@ -33,33 +33,33 @@ export class OfertaComponent implements OnInit, OnDestroy {
       })
 
     //definindo intervalo para o observable
-    let tempo = Observable.interval(2000)
+    // let tempo = Observable.interval(2000)
 
-    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
-      console.log(intervalo)
-    })
+    // this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
+    //   console.log(intervalo)
+    // })
 
     //observable (observável) -- analogia como event emitter
-    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
-      observer.next(1) //executado como primeiro parametro do subscribe
-      observer.next(3) //executado como primeiro parametro do subscribe
-      observer.complete() //executado como terceiro parametro do subscribe
-      observer.error('Erro durante o processo!') // //executado como segundo parametro do subscribe
-      observer.next(3) //executado como primeiro parametro do subscribe
-    })
+    // let meuObservableTeste = Observable.create((observer: Observer<number>) => {
+    //   observer.next(1) //executado como primeiro parametro do subscribe
+    //   observer.next(3) //executado como primeiro parametro do subscribe
+    //   observer.complete() //executado como terceiro parametro do subscribe
+    //   observer.error('Erro durante o processo!') //executado como segundo parametro do subscribe
+    //   observer.next(3) //executado como primeiro parametro do subscribe
+    // })
 
     //observable (observador) -- analogia com event listener
-    this.meuObservableTesteSubscription = meuObservableTeste.subscribe(
-      (resultado: number) => console.log(resultado + 10), //primeiro parametro do subscribe
-      (erro: string) => console.log(erro), //segundo parametro do subscribe
-      () => console.log('Stream de eventos foi finalizada') //terceiro parametro do subscribe
-    )
+    // this.meuObservableTesteSubscription = meuObservableTeste.subscribe(
+    //   (resultado: number) => console.log(resultado + 10), //primeiro parametro do subscribe
+    //   (erro: string) => console.log(erro), //segundo parametro do subscribe
+    //   () => console.log('Stream de eventos foi finalizada') //terceiro parametro do subscribe
+    // )
   }
 
   ngOnDestroy() {
     //encerra observables quando sai do escopo da página
-    this.meuObservableTesteSubscription.unsubscribe()
-    this.tempoObservableSubscription.unsubscribe()
+    // this.meuObservableTesteSubscription.unsubscribe()
+    // this.tempoObservableSubscription.unsubscribe()
   }
 
 }
