44710f50268a512a149bd1b53a2b104a86832084
matheus-lima92 <lima.eca@gmail.com>
2018-02-23 00:07:23 -0300

    added distinctUntilChanged in observable

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs/Subject'
 
 import 'rxjs/add/operator/switchMap'
 import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
 import 'rxjs/add/observable/of'
 
 import { OfertasService } from '../ofertas.service'
@@ -25,6 +26,7 @@ export class TopoComponent implements OnInit {
   ngOnInit() {
     this.ofertas = this.subjectPesquisa //retorno Oferta[]
       .debounceTime(1000) //executa ação do switchMap após 1 segundo
+      .distinctUntilChanged()
       .switchMap((termo: string) => {
 
         if(termo.trim() === ''){
