9c49441e4c5212a91784045701b7cf2e0e8be7a9
matheus-lima92 <lima.eca@gmail.com>
2018-03-02 16:18:57 -0300

    injecting http in service 'ordem-compra'

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,6 +1,12 @@
+import { Injectable } from '@angular/core' //necessario para injetar um serviço dentro de outro serviço
 import { Pedido } from './shared/pedido.model'
+import { Http } from '@angular/http'
 
+@Injectable()
 export class OrdemCompraService {
+
+	constructor (private http: Http) {}
+
     public efetivarCompra(pedido: Pedido): void {
         console.log(pedido)
     }
